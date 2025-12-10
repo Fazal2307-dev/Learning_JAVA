c407296e16562f7df0266747b3a3d8268a41b958
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-10 16:36:42 +0530

    All Commit

--- KunalDSA/Day1/Day1.java
+++ KunalDSA/Day1/Day1.java
@@ -1,5 +1,15 @@
-public class Day1{
-   public static void main(String[] args) {
-        System.out.println("Hello From KunalDSA Day1");
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your value: ");
+        int num = sc.nextInt();
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=num;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
