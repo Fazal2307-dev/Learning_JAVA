7bf3a6a478bdfa297a78b370958e4fdff48f4557
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-11 17:09:03 +0530

    Table Printing

---
+++ KunalDSA/Day1/Problem_4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Problem_4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter vaalue of num:");
+        int num = sc.nextInt();
+                for(int i=1;i<=10;i++){
+                    System.out.println(num + "*" +i+"="+ (num*i));
+                }
+    }
+}
