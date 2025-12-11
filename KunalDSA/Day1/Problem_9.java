51e67cf3438f40fff2074cb58113e825c62f5408
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-11 19:55:05 +0530

    Problem_9

--- KunalDSA/Day1/Problem_9.java
+++ KunalDSA/Day1/Problem_9.java
@@ -5,8 +5,14 @@ public class Problem_9 {
        System.out.print("Enter vaalue of n:");
        int n=sc.nextInt();
        for(int i=1;i<=n;i++){
+           for(int j=1;j<=n-i-1;j++){
+               System.out.print(" ");
+           }
            for(int j=1;j<=2*i-1;j++){
                System.out.print("*");
+           }
+           for(int j=1;j<=n-i-1;j++){
+               System.out.print(" ");
            }
            System.out.println();
        }
