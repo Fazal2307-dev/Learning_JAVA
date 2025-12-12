d9ba0a21b99119486958ca49e8688a4cadc891dc
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-12 18:08:40 +0530

    Problem_11

---
+++ KunalDSA/Day1/Problem_11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Problem_11{
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       System.out.print("enter value of n:");
+       int n = sc.nextInt();
+       for(int i=1;i<=n;i++){
+           System.out.println(2*i);
+       }
+       System.out.println();
+    }
+}
