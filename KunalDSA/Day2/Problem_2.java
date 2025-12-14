cb6ed2c01c143825e8aab8daeb4cc37b6b011a0a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-14 19:17:28 +0530

    Problem_3

---
+++ KunalDSA/Day2/Problem_2.java
@@ -0,0 +1,10 @@
+//sum of two number
+import java.util.Scanner;
+public class Problem_2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of a and b");
+        int a = sc.nextInt(),b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
---
+++ KunalDSA/Day2/Problem_3.java
@@ -0,0 +1,11 @@
+//check even or odd
+import java.util.Scanner;
+public class Problem_3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your value of n:");
+        int n=sc.nextInt();
+        System.out.println(n%2==0 ? "even":"odd");
+
+    }
+}
