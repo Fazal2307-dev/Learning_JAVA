564e4b0f4394197d0b610525d25db49fe6a48874
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-18 15:59:26 +0530

    Problem_1

---
+++ w3resource_Java/Basic_1/Problem_1.java
@@ -0,0 +1,12 @@
+/*
+Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov
+ */
+public class Problem_1{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("MD FAZAL");
+    }
+}
