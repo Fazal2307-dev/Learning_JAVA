8604e4ed3fda4ea0a1aca4fc9279aa199c82fc9e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-18 17:14:56 +0530

    Problem_5

---
+++ w3resource_Java/Basic_1/Problem_5.java
@@ -0,0 +1,13 @@
+//Write a Java program to print the sum (addition), multiply, subtract, divide
+// and remainder of two numbers.
+public class Problem_5{
+    public  static void main(String[] args){
+        System.out.println();
+        int a = 150;
+        int b=56;
+        //sum(addition)
+        int sum = a+b;
+        System.out.println("Sum of a and b:"+sum);
+
+    }
+}
