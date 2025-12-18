e5a01157b2b3794efc8a8ccd283c2917b79dc5a1
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-18 17:17:32 +0530

    Problem_5

--- w3resource_Java/Basic_1/Problem_5.java
+++ w3resource_Java/Basic_1/Problem_5.java
@@ -2,12 +2,16 @@
 // and remainder of two numbers.
 public class Problem_5{
     public  static void main(String[] args){
-        System.out.println();
         int a = 150;
         int b=56;
         //sum(addition)
         int sum = a+b;
         System.out.println("Sum of a and b:"+sum);
-
+        //multiply
+        int multiply = a *b;
+        System.out.println("Multiplication of a and b:"+multiply);
+        //subtraction
+        int subtraction = a-b;
+        System.out.println("subtraction of a and b:"+subtraction);
     }
 }
