ffca854eab9cf8f7f0e70802ed99a58c4c7f2e77
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-18 16:57:38 +0530

    Problem_4

--- w3resource_Java/Basic_1/Problem_4.java
+++ w3resource_Java/Basic_1/Problem_4.java
@@ -8,5 +8,6 @@ public class Problem_4{
         //c. 20 + -3*5 / 8
         System.out.println(20 + -3*5 / 8);
         //d. 5 + 15 / 3 * 2 - 8 % 3
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
     }
 }
