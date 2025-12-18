fa1744a5dc042a5f4c2738d9204a87d7ad3225d7
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-18 16:55:49 +0530

    Problem_4

---
+++ w3resource_Java/Basic_1/Problem_4.java
@@ -0,0 +1,8 @@
+//Write a Java program to print the results of the following operations.
+public class Problem_4{
+    public static void main(String[] args){
+        //a. -5 + 8 * 6
+        System.out.println(-5 + 8 * 6);
+        //
+    }
+}
