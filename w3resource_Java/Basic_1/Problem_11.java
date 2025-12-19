3cc6be9ecc27ca4b76cd9c928c8ca81ad7772865
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-19 19:37:26 +0530

    Problem_11

---
+++ w3resource_Java/Basic_1/Problem_11.java
@@ -0,0 +1,23 @@
+//print america flag
+public class Problem_11{
+    public static void main(String[] args){
+        System.out.println("* * * * * * ===================");
+        System.out.println(" * * * * *  ===================");
+        System.out.println("* * * * * * ===================");
+        System.out.println(" * * * * *  ===================");
+        System.out.println("* * * * * * ===================");
+        System.out.println(" * * * * *  ===================");
+        System.out.println("* * * * * * ===================");
+        System.out.println(" * * * * *  ===================");
+        System.out.println("* * * * * * ===================");
+        System.out.println(" * * * * *  ===================");
+        System.out.println("===============================");
+        System.out.println("===============================");
+        System.out.println("===============================");
+        System.out.println("===============================");
+        System.out.println("===============================");
+        System.out.println("===============================");
+        System.out.println("===============================");
+
+    }
+}
