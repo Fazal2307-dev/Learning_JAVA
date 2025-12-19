155414050731282de2ee15a0c92292d5ac7907b3
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-19 15:45:30 +0530

    PRoblem_8

---
+++ w3resource_Java/Basic_1/Problem_8.java
@@ -0,0 +1,8 @@
+//Write a Java program to print the area and perimeter of a circle.
+public class Problem_8{
+    public static void main(String[] args){
+        double Radius = 7.5;
+        //Perimeter of circle
+        System.out.println("Perimeter of circle:"+(2*3.14*Radius));
+    }
+}
