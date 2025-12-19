d6b2f79acbc88d2b92d342843be8b7d5643cebad
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-19 19:21:49 +0530

    Problem_10

---
+++ w3resource_Java/Basic_1/Problem_10.java
@@ -0,0 +1,12 @@
+//Write a Java program to print the area and perimeter of a rectangle.
+import java.util.Scanner;
+public class Problem_10{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of length and breadth:");
+        int length = sc.nextInt(),breadth =sc.nextInt();
+        //Area of rectangle
+        int AreaOfRectangle = length * breadth;
+        System.out.print("Area of Rectangle:"+AreaOfRectangle);
+    }
+}
