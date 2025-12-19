4da8be8e740e530183737c738b0149ea3bebccc2
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-19 15:41:07 +0530

    Problem_7

---
+++ w3resource_Java/Basic_1/Problem_7.java
@@ -0,0 +1,7 @@
+//Write a Java program to compute the specified expressions and print the output.
+public class Problem_7{
+    public static void main(String[] args){
+        double result =  4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println("Result"+result);
+    }
+}
