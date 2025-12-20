a8ca4e841dc3a37d7fca80164b09165bd4ab4b82
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-20 10:27:42 +0530

    Problem_13

---
+++ w3resource_Java/Basic_1/Problem_13.java
@@ -0,0 +1,15 @@
+//Write a Java program to print a face.
+//+"""""+
+//[| o o |]
+// |  ^  |
+// | '-' |
+// +-----+
+public class Problem_13{
+    public static void main(String[] args){
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
