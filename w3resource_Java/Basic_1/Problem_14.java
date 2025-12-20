6463e9b26d2d1309a7c5c2dcd6cf3d15c8020d10
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-20 12:00:31 +0530

    PRoblem_14

--- w3resource_Java/Basic_1/Problem_14.java
+++ w3resource_Java/Basic_1/Problem_14.java
@@ -4,5 +4,6 @@ public class Problem_14{
     public static void main(String[] args) {
         System.out.println("\nJava Version :"+System.getProperty("java.version"));
         System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
+        System.out.println("Java Home: " + System.getProperty("java.home"));
     }
 }
