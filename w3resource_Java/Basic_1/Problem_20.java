c0349b4ec04eca6a8dc1c831dbaf94abda5472d4
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-21 21:43:58 +0530

    PRoblem_20

--- w3resource_Java/Basic_1/Problem_20.java
+++ w3resource_Java/Basic_1/Problem_20.java
@@ -12,5 +12,10 @@ public class Problem_20{
             if(i % 5 ==0)
                 System.out.print(i+", ");
         }
+        System.out.println("\n Divisible by 3 & 5:");
+        for(int i =1;i<=100;i++){
+            if(i % 3 ==0 && i%5==0)
+                System.out.print(i+", ");
+        }
     }
 }
