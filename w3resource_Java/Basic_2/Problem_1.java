b5073709c5d13b9513dd47620403538bda2d787f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-23 18:42:59 +0530

    Problem_1

---
+++ w3resource_Java/Basic_2/Problem_1.java
@@ -0,0 +1,7 @@
+//Write a Java program to find the value of a specified expression
+public class Problem_1{
+    public static void main(String[] args){
+        //a) 101 + 0) / 3
+        System.out.print( (101 + 0) / 3);
+    }
+}
