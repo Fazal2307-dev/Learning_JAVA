6f3b4cec3bdc4e51b2e7b441bfce990c2f29dbf3
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-25 21:58:08 +0530

    Problem_7

---
+++ w3resource_Java/Methods/Problem_7.java
@@ -0,0 +1,10 @@
+//Java program to demonstrate how to create and use a method.
+public class Problem_7{
+    public void printMessage(){
+        System.out.print("Hello world!");
+    }
+    public static void main(String[] args){
+        Problem_7 obj = new Problem_7();
+        obj.printMessage();
+    }
+}
