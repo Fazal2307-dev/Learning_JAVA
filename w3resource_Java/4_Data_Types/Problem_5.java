06955ad9379c4716293ee2b7481b5bac771d65b9
Fazal2307-dev <mdfazal2307@gmail.com>
2025-12-29 19:07:07 +0530

    Problem_5

---
+++ w3resource_Java/4_Data_Types/Problem_5.java
@@ -0,0 +1,11 @@
+//Write a Java program that reads a number and displays the square,
+// cube, and fourth power.
+import java.util.Scanner;
+public class Problem_5{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        int n = in.nextInt();
+        System.out.println("Square of n:"+n*n);
+    }
+}
