8ba087daa8df3148fe413ec82f75b84f834bcba3
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-12 19:06:19 +0530

    Problem_94

---
+++ w3resource_Java/2_Basic_2/Problem_94.java
@@ -0,0 +1,28 @@
+//Write a Java program that accepts three integers and checks whether
+// the sum of the first two integers is greater than the third integer.
+// Three integers are in the interval [-231, 231 ].
+import java.util.Scanner;
+
+// Defining a class named "Main"
+public class Problem_94{
+
+    // Main method, the entry point of the program
+    public static void main(String[] args) {
+        // Creating a Scanner object for reading user input
+        Scanner in = new Scanner(System.in);
+
+        // Prompting the user to input three integers (a, b, c)
+        System.out.println("Input three integers (a, b, c):");
+
+        // Reading long integers a, b, and c from the user
+        long a = in.nextLong();
+        long b = in.nextLong();
+        long c = in.nextLong();
+
+        // Prompting the user with a message
+        System.out.println("Check whether (a + b) is greater than c?");
+
+        // Checking and printing whether the sum of a and b is greater than c
+        System.out.println(a + b > c);
+    }
+}
