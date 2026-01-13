a6d99d227b1309689937f0ef4affc2a4eb198983
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-13 18:21:29 +0530

    Problem_28

---
+++ w3resource_Java/1_Basic_1/Problem_28.java
@@ -0,0 +1,7 @@
+//Write a Java program to display system time.
+import java.util.*;
+public class Problem_28{
+    public static void main(String[] args){
+        System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+    }
+}
