7f7199266f3e90a6b63a4f3ec8ce5338ff495fba
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-13 18:30:15 +0530

    Problem_32

---
+++ w3resource_Java/1_Basic_1/Problem_32.java
@@ -0,0 +1,19 @@
+//Write a Java program to rotate an array (length 3) of integers in
+// the left direction.
+import java.util.Arrays;
+
+public class Problem_32 {
+    public static void main(String[] args) {
+        // Define an integer array, array_nums
+        int[] array_nums = {20, 30, 40};
+
+        // Print the elements of the original array
+        System.out.println("Original Array: " + Arrays.toString(array_nums));
+
+        // Create a new array by rotating the elements of the original array
+        int[] new_array_nums = {array_nums[1], array_nums[2], array_nums[0]};
+
+        // Print the elements of the rotated array
+        System.out.println("Rotated Array: " + Arrays.toString(new_array_nums));
+    }
+}
