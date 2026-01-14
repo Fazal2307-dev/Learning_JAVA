86b5c938b1bcd558a58feeffc59cf1027d50ef88
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-14 19:09:45 +0530

    Problem_46

---
+++ w3resource_Java/1_Basic_1/Problem_46.java
@@ -0,0 +1,19 @@
+//Write a Java program to get the value of environment variables PATH,
+// TEMP, USERNAME.
+import java.lang.*;
+
+public class Problem_46{
+    public static void main(String[] args) {
+        // Display the value of the specified environment variable "PATH"
+        System.out.println("\nEnvironment variable PATH: ");
+        System.out.println(System.getenv("PATH"));
+
+        // Display the value of the specified environment variable "TEMP"
+        System.out.println("\nEnvironment variable TEMP: ");
+        System.out.println(System.getenv("TEMP"));
+
+        // Display the value of the specified environment variable "USERNAME"
+        System.out.println("\nEnvironment variable USERNAME: ");
+        System.out.println(System.getenv("USERNAME"));
+    }
+}
