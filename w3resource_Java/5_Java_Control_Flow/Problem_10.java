9e8013450573b56ae4491ee47436899f683c9b2c
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-15 20:57:33 +0530

    Problem_10

---
+++ w3resource_Java/5_Java_Control_Flow/Problem_10.java
@@ -0,0 +1,11 @@
+//Write a Java program to display the first 10 natural numbers.
+import java.util.*;
+public class Problem_10{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Print 10 natural number:");
+        for(int i =1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
