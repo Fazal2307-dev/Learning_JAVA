36e2cd4d142a72f9e667f03d6008cc564c82c619
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-16 17:55:16 +0530

    Problem_30

---
+++ w3resource_Java/5_Java_Control_Flow/Problem_30.java
@@ -0,0 +1,21 @@
+//Write a Java program that accepts two floating­point numbers and checks whether they
+// are the same up to two decimal places.
+import java.util.Scanner;
+public class Problem_30 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input first floating­point number: ");
+        double num1 = input.nextDouble();
+        System.out.print("Input second floating­point number: ");
+        double num2 = input.nextDouble();
+        input.close();
+
+        if (Math.abs(num1 - num2) <= 0.01) {
+            System.out.println("These numbers are the same.");
+        }
+        else {
+            System.out.println("These numbers are different.");
+        }
+    }
+}
