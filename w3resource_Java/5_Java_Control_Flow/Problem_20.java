88778f711f7861520d8be8f55797a42d9379f777
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-16 17:39:04 +0530

    Problem_20

---
+++ w3resource_Java/5_Java_Control_Flow/Problem_20.java
@@ -0,0 +1,21 @@
+//Write a Java program to print Floyd's Triangle.
+import java.util.Scanner;
+public class Problem_20{
+    public static void main(String[] args)
+    {
+        int numberOfRows;
+        System.out.print("Input number of rows : ");
+        Scanner in = new Scanner(System.in);
+        numberOfRows = in.nextInt();
+        int number = 1;
+        for (int row = 1; row <= numberOfRows; row++)
+        {
+            for (int column = 1; column <= row; column++)
+            {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
