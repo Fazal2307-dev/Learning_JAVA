731d1c95d663b08e86a165967770f79e3c3e497b
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-18 21:44:11 +0530

    Problem_2

---
+++ w3resource_Java/7_Math/Problem_2.java
@@ -0,0 +1,13 @@
+//Write a Java program to get whole and fractional parts from a double value.
+import java.util.*;
+public class Problem_2 {
+    public static void main(String[] args) {
+        double value = 12.56;
+        double fractional_part = value % 1;
+        double integral_part = value - fractional_part;
+        System.out.print("\nOriginal value: "+value);
+        System.out.print("\nIntegral part: "+integral_part);
+        System.out.print("\nFractional part: "+fractional_part);
+        System.out.println();
+    }
+}
