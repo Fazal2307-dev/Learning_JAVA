7e900e2352a4f3aadeb82a7a3b64060f9e02a6bf
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-18 21:52:30 +0530

    PRoblem_8

---
+++ w3resource_Java/7_Math/Problem_8.java
@@ -0,0 +1,17 @@
+//Write a Java program to convert an integer value to an absolute value.
+import java.util.*;
+public class Problem_8 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input an integer number: ");
+        int x = in.nextInt();
+        System.out.printf("The absolute value of %d is: %d",x, convert (x));
+        System.out.printf("\n");
+    }
+    public static int convert (int n)
+    {
+        int absvalue = (n >= 0) ? n : -n;
+        return absvalue;
+    }
+}
