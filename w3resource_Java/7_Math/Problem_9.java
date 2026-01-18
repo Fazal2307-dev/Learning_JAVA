5ee590e950ce2cccb24657e8000ed3322a60aa32
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-18 21:53:47 +0530

    Problem_9

---
+++ w3resource_Java/7_Math/Problem_9.java
@@ -0,0 +1,19 @@
+//Write a Java program to convert a floating value to an absolute value.
+import java.util.*;
+public class Problem_9 {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a float number: ");
+        float  x = in.nextFloat();
+        System.out.printf("The absolute value of %.2f is: %.2f",x, convert(x));
+        System.out.printf("\n");
+    }
+
+    public static float convert(float n)
+    {
+        float absvalue = (n >= 0) ? n : -n;
+        return absvalue;
+    }
+}
