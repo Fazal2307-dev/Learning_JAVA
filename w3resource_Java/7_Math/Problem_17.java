b30f98594b99be26628f1349d92775bf679df72a
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-19 13:44:07 +0530

    Problem_17

---
+++ w3resource_Java/7_Math/Problem_17.java
@@ -0,0 +1,31 @@
+//Write a Java program to calculate and print the average (or mean) of the
+// stream of given numbers.
+import java.util.*;
+public class Problem_17 {
+    // Prints average of a stream of numbers
+    static void streamAvg(float arr[])
+    {
+        int n = arr.length;
+        float avg = 0;
+        for (int i = 0; i < n; i++)
+        {
+            //avg = getAvg(avg, arr[i], i);
+            avg = (avg * i + arr[i]) / (i + 1);
+            System.out.printf("Average of %d numbers is %f \n",
+                    i + 1, avg);
+        }
+        return;
+    }
+
+    // Calculate the new average
+    static float getAvg(float prev_avg, float x, int n)
+    {
+        return (prev_avg * n + x) / (n + 1);
+    }
+
+    public static void main(String[] args)
+    {
+        float arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        streamAvg(arr);
+    }
+}
