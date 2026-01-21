0b045bc2850fa878064aabfdb7e187dcdabb4626
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-21 12:29:23 +0530

    Problem_14

---
+++ w3resource_Java/8_Numbers/Problem_14.java
@@ -0,0 +1,18 @@
+//Write a Java program to check whether a number is an automorphic number or not.
+import java.util.*;
+public class Problem_14{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a number : ");
+        int num = sc.nextInt();
+        int sq_num = num*num;
+
+        String str_num = Integer.toString(num);
+        String square = Integer.toString(sq_num);
+
+        if(square.endsWith(str_num))
+            System.out.println("Automorphic Number.");
+        else
+            System.out.println("Not an Automorphic Number.");
+    }
+}
