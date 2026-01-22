8fa864270e91d4455251804b5954200697d214fd
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-22 18:15:36 +0530

    PRoblem_10

---
+++ w3resource_Java/10_Strings/Problem_10.java
@@ -0,0 +1,17 @@
+//Write a Java program to compare a given string to a specified string buffer.
+public class Problem_10 {
+    // Define the main method.
+    public static void main(String[] args) {
+        // Declare and initialize two string variables, str1 and str2.
+        String str1 = "example.com", str2 = "Example.com";
+
+        // Create a StringBuffer object strbuf initialized with the value of str1.
+        StringBuffer strbuf = new StringBuffer(str1);
+
+        // Compare str1 and strbuf for content equality and print the result.
+        System.out.println("Comparing " + str1 + " and " + strbuf + ": " + str1.contentEquals(strbuf));
+
+        // Compare str2 and strbuf for content equality and print the result.
+        System.out.println("Comparing " + str2 + " and " + strbuf + ": " + str2.contentEquals(strbuf));
+    }
+}
