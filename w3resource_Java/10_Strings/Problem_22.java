99210b1f894f68078f0c7df0019a29b28a9acc86
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-23 17:59:55 +0530

    PRoblem_21

---
+++ w3resource_Java/10_Strings/Problem_22.java
@@ -0,0 +1,14 @@
+//Write a Java program to get the length of a given string.
+public class Problem_22 {
+    // Define the main method.
+    public static void main(String[] args) {
+        // Declare and initialize a string variable.
+        String str = "example.com";
+
+        // Get the length of the string 'str'.
+        int len = str.length();
+
+        // Display the length of the string.
+        System.out.println("The string length of '" + str + "' is: " + len);
+    }
+}
