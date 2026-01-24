d2a8537fba8a7b3ca58fe25eb9532abbf460d4bc
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-24 17:10:15 +0530

    PRoblem_29

---
+++ w3resource_Java/10_Strings/Problem_29.java
@@ -0,0 +1,16 @@
+//Write a Java program to convert all characters in a string to lowercase.
+public class Problem_29 {
+
+    // Define the main method.
+    public static void main(String[] args) {
+        // Declare and initialize a string variable.
+        String str = "The Quick BroWn FoX!";
+
+        // Convert the above string to all lowercase.
+        String lowerStr = str.toLowerCase();
+
+        // Display the original and lowercase strings for comparison.
+        System.out.println("Original String: " + str);
+        System.out.println("String in lowercase: " + lowerStr);
+    }
+}
