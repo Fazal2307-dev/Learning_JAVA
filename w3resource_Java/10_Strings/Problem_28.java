b2dbf8d9b7dc46ef794f1050fb6683846a6ed999
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-24 17:09:05 +0530

    PRoblem_28

---
+++ w3resource_Java/10_Strings/Problem_28.java
@@ -0,0 +1,15 @@
+//Write a Java program to create a character array containing a string.
+public class Problem_28 {
+
+    // Define the main method.
+    public static void main(String[] args) {
+        // Declare and initialize a string variable.
+        String str = "Java Exercises.";
+
+        // Convert the above string to a char array.
+        char[] arr = str.toCharArray();
+
+        // Display the contents of the char array.
+        System.out.println(arr);
+    }
+}
