e4dcba53c6930114e8e3437e2e076d6da235cecb
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-27 18:19:09 +0530

    PRoblem_61

---
+++ w3resource_Java/10_Strings/Problem_61.java
@@ -0,0 +1,25 @@
+//Write a Java program to create a new string taking specified number of characters
+// from first and last position of a given string.
+import java.util.*;
+
+// Define a class named Main
+public class Problem_61{
+
+    // Method to return a string formed by taking 'n' characters from the beginning and 'n' characters from the end
+    public String nTwice(String str, int n) {
+        return str.substring(0, n) + str.substring(str.length() - n, str.length());
+    }
+
+    // Main method to execute the program
+    public static void main(String[] args) {
+        Problem_61 m = new Problem_61(); // Create an instance of the Main class
+
+        String str1 = "Welcome"; // String
+        int n1 = 3; // Integer 'n'
+
+        // Display the given string, the given number, and the new string formed using nTwice method
+        System.out.println("The given strings is: " + str1);
+        System.out.println("The given numbers is: " + n1);
+        System.out.println("The new string is: " + m.nTwice(str1, n1));
+    }
+}
