4652dc2a7459b16c371499a43a981d8f369ec69a
Fazal2307-dev <mdfazal2307@gmail.com>
2026-01-28 12:36:57 +0530

    PRoblem_67

---
+++ w3resource_Java/10_Strings/Problem_67.java
@@ -0,0 +1,37 @@
+//Write a Java program to read a string and remove the first two characters if one or
+// both are equal to a specified character, otherwise leave them unchanged.
+import java.util.*;
+
+// Define a class named Main
+public class Problem_67{
+
+    // Method to exclude 'x' characters at the first two positions from the input string
+    public String exceptTwoO(String stng) {
+        String temp = ""; // Create an empty string
+
+        // Iterate through each character in the input string
+        for (int i = 0; i < stng.length(); i++) {
+            // If the current index is 0 and the character is not 'x', append the character to the temporary string
+            if (i == 0 && stng.charAt(i) != 'x')
+                temp += stng.charAt(i);
+                // If the current index is 1 and the character is not 'x', append the character to the temporary string
+            else if (i == 1 && stng.charAt(i) != 'x')
+                temp += stng.charAt(i);
+                // If the current index is greater than 1, append the character to the temporary string
+            else if (i > 1)
+                temp += stng.charAt(i);
+        }
+        return temp; // Return the modified string
+    }
+
+    // Main method to execute the program
+    public static void main(String[] args) {
+        Problem_67 m = new Problem_67(); // Create an instance of the Main class
+
+        String str1 = "xxcyte"; // Input string
+
+        // Display the given string and the new string using exceptTwoO method
+        System.out.println("The given strings is: " + str1);
+        System.out.println("The new string is: " + m.exceptTwoO(str1));
+    }
+}
