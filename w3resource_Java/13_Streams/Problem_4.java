22355ddcdc058d73ca1867a85c1e6f0dd16d8a03
Fazal2307-dev <mdfazal2307@gmail.com>
2026-02-16 17:44:31 +0530

    Problem_4

---
+++ w3resource_Java/13_Streams/Problem_4.java
@@ -0,0 +1,16 @@
+//Write a Java program to remove all duplicate elements from a list using streams.
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Problem_4 {
+    public static void main(String[] args) {
+        List < Integer > nums = Arrays.asList(10, 23, 22, 23, 24, 24, 33, 15, 26, 15);
+        System.out.println("Original List of numbers: " + nums);
+        // Remove duplicates
+        List < Integer > distinctNumbers = nums.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("After removing duplicates from the said list: " + distinctNumbers);
+    }
+}
