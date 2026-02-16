1c5a2f6027677abd57a55bfdec3b58ff2107c46b
Fazal2307-dev <mdfazal2307@gmail.com>
2026-02-16 17:45:41 +0530

    PRoblem_5

---
+++ w3resource_Java/13_Streams/Problem_5.java
@@ -0,0 +1,23 @@
+//Write a Java program to count the number of strings in a list that start with a
+// specific letter using streams.
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem_5{
+    public static void main(String[] args) {
+        List < String > colors = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown");
+        System.out.println("Original list of strings (colors): " + colors);
+        char startingLetter = 'B';
+        // Count strings starting with a specific letter
+        long ctr = colors.stream()
+                .filter(s -> s.startsWith(String.valueOf(startingLetter)))
+                .count();
+        System.out.println("\nNumber of colors starting with '" + startingLetter + "': " + ctr);
+        char startingLetter1 = 'Y';
+        // Count strings starting with a specific letter
+        ctr = colors.stream()
+                .filter(s -> s.startsWith(String.valueOf(startingLetter1)))
+                .count();
+        System.out.println("\nNumber of colors starting with '" + startingLetter1 + "': " + ctr);
+    }
+}
