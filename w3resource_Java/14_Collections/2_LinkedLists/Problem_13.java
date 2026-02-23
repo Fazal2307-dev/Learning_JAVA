45993173fb1a62e7752e7b2bd6b61ecd04753ee6
Fazal2307-dev <mdfazal2307@gmail.com>
2026-02-23 17:59:59 +0530

    PRoblem_13

---
+++ w3resource_Java/14_Collections/2_LinkedLists/Problem_13.java
@@ -0,0 +1,27 @@
+//Write a Java program to remove the first and last elements from a linked list.
+import java.util.*;
+
+public class Problem_13 {
+    public static void main(String[] args) {
+        // create an empty linked list
+        LinkedList<String> l_list = new LinkedList<String>();
+        // use add() method to add values in the linked list
+        l_list.add("Red");
+        l_list.add("Green");
+        l_list.add("Black");
+        l_list.add("Pink");
+        l_list.add("orange");
+
+        // print the list
+        System.out.println("The Original linked list: " + l_list);
+
+        // Remove the first element
+        Object firstElement = l_list.removeFirst();
+        System.out.println("Element removed: "+ firstElement);
+
+        // Remove the last element
+        Object lastElement = l_list.removeLast();
+        System.out.println("Element removed: "+ lastElement);
+        System.out.println("The New linked list: " + l_list);
+    }
+}
