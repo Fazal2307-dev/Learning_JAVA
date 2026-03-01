812c4ff9846036e4ab3da1cf7ce1fd8fbac14d91
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-01 17:15:33 +0530

    PRoblem_13

---
+++ w3resource_Java/14_Collections/4_TreeSet/Problem_13.java
@@ -0,0 +1,26 @@
+//Write a Java program to get an element in a tree set that has a lower value than
+// the given element.
+import java.util.TreeSet;
+import java.util.Iterator;
+
+public class Problem_13 {
+    public static void main(String[] args) {
+        // creating TreeSet
+        TreeSet <Integer>tree_num = new TreeSet<Integer>();
+        TreeSet <Integer>treeheadset = new TreeSet<Integer>();
+
+        // Add numbers in the tree
+        tree_num.add(10);
+        tree_num.add(22);
+        tree_num.add(36);
+        tree_num.add(25);
+        tree_num.add(16);
+        tree_num.add(70);
+        tree_num.add(82);
+        tree_num.add(89);
+        tree_num.add(14);
+
+        System.out.println("Strictly less than 69 : "+tree_num.lower(69));
+        System.out.println("Strictly less than 12 : "+tree_num.lower(12));
+    }
+}
