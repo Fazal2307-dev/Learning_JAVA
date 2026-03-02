aab4e878382354e0dde96452cc6d0d216aaf0a5f
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-02 14:18:49 +0530

    PRoblem_5

---
+++ w3resource_Java/14_Collections/5_PriorityQueue/Problem_5.java
@@ -0,0 +1,19 @@
+//Write a Java program to remove all elements from a priority queue.
+import java.util.*;
+public class Problem_5 {
+    public static void main(String[] args) {
+        // Create Priority Queue
+        PriorityQueue<String> pq1 = new PriorityQueue<String>();
+        // use add() method to add values in the Priority Queue
+        pq1.add("Red");
+        pq1.add("Green");
+        pq1.add("Black");
+        pq1.add("White");
+        System.out.println("Original Priority Queue: "+pq1);
+
+        // Removing all the elements from the Priority Queue
+        pq1.clear();
+
+        System.out.println("The New Priority Queue: " + pq1);
+    }
+}
