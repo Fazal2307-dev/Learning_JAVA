f396d01dbfdf1d6caf4e579ff40575afe69f4485
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-02 14:12:49 +0530

    Problem_2

---
+++ w3resource_Java/14_Collections/5_PriorityQueue/Problem_2.java
@@ -0,0 +1,17 @@
+//Write a Java program to iterate through all elements in the priority queue.
+import java.util.PriorityQueue;
+public class Problem_2 {
+    public static void main(String[] args) {
+        PriorityQueue<String> pq = new PriorityQueue<String>();
+        pq.add("Red");
+        pq.add("Green");
+        pq.add("Orange");
+        pq.add("White");
+        pq.add("Black");
+        System.out.println("Elements of the Priority Queue: ");
+        // iterate the Priority Queue
+        for (String element : pq) {
+            System.out.println(element);
+        }
+    }
+}
