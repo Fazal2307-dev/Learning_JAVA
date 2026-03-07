ae1feaf019ff511d2152a930702dcf10737e22f9
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-07 16:01:19 +0530

    Problem_16

---
+++ w3resource_Java/14_Collections/7_TreeMap/Problem_16.java
@@ -0,0 +1,26 @@
+//Write a Java program to get a key-value mapping associated with the greatest key
+// strictly less than the given key. Return null if there is no such key.
+import java.util.*;
+import java.util.Map.Entry;
+public class Problem_16 {
+    public static void main(String args[]){
+
+        // Create a tree map
+        TreeMap< Integer, String > tree_map1 = new TreeMap< Integer, String >();
+
+        // Put elements to the map
+        tree_map1.put(10, "Red");
+        tree_map1.put(20, "Green");
+        tree_map1.put(40, "Black");
+        tree_map1.put(50, "White");
+        tree_map1.put(60, "Pink");
+
+        System.out.println("Orginal TreeMap content: "+tree_map1);
+        System.out.println("Checking the entry for 10: ");
+        System.out.println("Key(s): "+tree_map1.lowerEntry(10));
+        System.out.println("Checking the entry for 20: ");
+        System.out.println("Key(s): "+tree_map1.lowerEntry(20));
+        System.out.println("Checking the entry for 70: ");
+        System.out.println("Key(s): "+tree_map1.lowerEntry(70));
+    }
+}
