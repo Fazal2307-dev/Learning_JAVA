b6e16b8d24e7290f36004c356d9f203a04f5e4bc
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-09 18:58:34 +0530

    Problem_Hello_world_thread

---
+++ w3resource_Java/15_Java_Thread/Hello_world_thread.java
@@ -0,0 +1,13 @@
+//Write a Java program to create a basic Java thread that prints "Hello, World!"
+// when executed.
+
+public class Hello_world_thread extends Thread {
+  @Override
+  public void run() {
+    System.out.println("Hello, World!");
+  }
+  public static void main(String[] args) {
+    Hello_world_thread thread = new Hello_world_thread();
+    thread.start();
+  }
+}
