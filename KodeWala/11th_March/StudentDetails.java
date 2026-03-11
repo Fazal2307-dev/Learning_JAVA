f34796a5623d3242ca62301d97098d0a0a7a0867
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-11 19:12:50 +0530

    StudentDetails

---
+++ KodeWala/11th_March/StudentDetails.java
@@ -0,0 +1,9 @@
+//write a programme to print StudentDetails
+public class StudentDetails{
+    public static void main(String[] args){
+        System.out.println("Name  : MD FAZAL");
+        System.out.println("State : Bihar");
+        System.out.println("DOB   : 23/07/2002");
+        System.out.println("Degree: B.Tech");
+    }
+}
