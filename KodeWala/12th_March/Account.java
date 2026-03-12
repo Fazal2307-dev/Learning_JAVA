aa4041799e4b4e1f965344dc6a4bb22eeb44ece2
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-12 18:57:24 +0530

    AccountDetails

---
+++ KodeWala/12th_March/Account.java
@@ -0,0 +1,9 @@
+//write a programme to print AccountDetails
+class AccountDetails{
+    public static void main(String args[]){
+        System.out.println("AccountName : SBI_BARH");
+        System.out.println("AccountNo   : 1236547987");
+        System.out.println("AccountHoulder:MD FAZAL");
+        System.out.println("IFSC CODE : SBIN001456");
+    }
+}
