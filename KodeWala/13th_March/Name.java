1034f168f69b9f893e7c65a662b950db4324bd73
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-13 18:19:05 +0530

    Name

---
+++ KodeWala/13th_March/Name.java
@@ -0,0 +1,9 @@
+//write a programme to print a name through cmd line
+class Name{
+    public static void main(String[] args){
+        String FirstName = args[0];
+        String SecondName = args[1];
+        System.out.println("FirstName:"+FirstName);
+        System.out.println("SecondName:"+SecondName);
+    }
+}
