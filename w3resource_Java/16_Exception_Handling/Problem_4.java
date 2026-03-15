d3970769718c999ae228c87bc22f3ec36610bb6d
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-15 18:10:28 +0530

    PRoblem_$

---
+++ w3resource_Java/16_Exception_Handling/Problem_4.java
@@ -0,0 +1,33 @@
+//Write a Java program that reads a list of numbers from a file and throws an exception
+// if any of the numbers are positive.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Problem_4 {
+    public static void main(String[] args) {
+        try {
+            checkNumbersFromFile("test.txt");
+            System.out.println("All numbers are non-positive.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (PositiveNumberException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static void checkNumbersFromFile(String fileName) throws FileNotFoundException, PositiveNumberException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number > 0) {
+                throw new PositiveNumberException("Positive number found: " + number);
+            }
+        }
+        scanner.close();
+    }
+}
+class PositiveNumberException extends Exception {
+    public PositiveNumberException(String message) {
+        super(message);
+    }
+}
