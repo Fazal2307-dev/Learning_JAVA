9db912c10923ef92714c73c3e25e0747f3d20579
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-17 20:43:23 +0530

    String_Is_Empty

---
+++ w3resource_Java/17_Java_Lembda_Expression/String_Is_Empty.java
@@ -0,0 +1,21 @@
+//Write a Java program to implement a lambda expression to check if a given string is
+// empty.
+import java.util.function.Predicate;
+
+public class String_Is_Empty {
+    public static void main(String[] args) {
+        // Lambda expression to check if a given string is empty
+        Predicate<String> isEmptyString = str -> str.isEmpty();
+
+        // Test cases
+        String str1 = ""; // empty string
+        String str2 = "Java lambda expression!"; // non-empty string
+
+        // Check if the strings are empty using the lambda expression
+        System.out.println("String 1: \"" + str1 + "\"");
+        System.out.println("String 1 is empty: " + isEmptyString.test(str1));
+
+        System.out.println("\nString 2: \"" + str2 + "\"");
+        System.out.println("String 2 is empty: " + isEmptyString.test(str2));
+    }
+}
