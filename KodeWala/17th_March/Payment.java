9a9b6ac7e8c656ce2dd839095df62ee317e0f0e1
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-17 20:33:02 +0530

    Payment

---
+++ KodeWala/17th_March/Payment.java
@@ -0,0 +1,10 @@
+//write a programme to print Payment method
+class Payment{
+    static int  balance = 1000;
+    public static void main(String args[]){
+        String name = "KodeWala";
+        balance = balance+100;
+        System.out.println("Account balance is:"+balance);
+
+    }
+}
