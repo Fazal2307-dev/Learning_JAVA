8b524c4e58793281332d53a6cf625a2a5cbc6945
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-18 14:16:36 +0530

    College

---
+++ KodeWala/18th_March/College.java
@@ -0,0 +1,13 @@
+//write a programme about college
+class College
+{
+    static String collegeName = "IIT";
+     static String studentName = "MD FAZAL";
+    public static void main(String args[])
+    {
+        String studentId = "12019464";
+        System.out.println("collegeName:"+collegeName);
+        System.out.println("studentName:"+studentName);
+        System.out.println("studentId:"+studentId);
+    }
+}
