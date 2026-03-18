61f59103943adeeecf809565db5036a4d8a2b406
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-18 14:14:32 +0530

    Banking

---
+++ KodeWala/18th_March/Banking.java
@@ -0,0 +1,18 @@
+//write a programme about Banking
+class SBI
+{
+    static int Amount = 1000;
+    static String name ="Fazal";
+
+
+    public static void main(String args[])
+    {
+         String str;
+        System.out.println("amount is:"+Amount);
+        System.out.println("Name is:"+name);
+        System.out.println("Str is:"+str);
+
+
+
+    }
+}
