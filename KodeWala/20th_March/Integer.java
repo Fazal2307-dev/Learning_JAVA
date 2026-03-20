0f91b25c7cc8f772da6c285de590f0544632cb03
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-20 22:28:01 +0530

    Integer

---
+++ KodeWala/20th_March/Integer.java
@@ -0,0 +1,8 @@
+public class Integer{
+    public static void main(String[] args){
+        int pen =100;
+        int pencil = 150;
+        int total = pen + pencil;
+        System.out.println("Total item:"+total);
+    }
+}
