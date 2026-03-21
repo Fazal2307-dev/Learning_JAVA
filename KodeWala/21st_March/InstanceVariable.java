265cad127c8a60e4552aff84e50c1f9a3f0892cf
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-21 20:16:15 +0530

    InstanceVariable

---
+++ KodeWala/21st_March/InstanceVariable.java
@@ -0,0 +1,7 @@
+public class InstanceVariable {
+    int balance =100;
+    public static void main(String[] args){
+        InstanceVariable instanceVariable = new InstanceVariable();
+        System.out.println("Balance:"+instanceVariable.balance);
+    }
+}
