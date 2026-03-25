e3cf8d550062706b1f1e054f737a903f5b36a5a9
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-25 21:36:56 +0530

    SumOfValue

---
+++ KodeWala/25th_March/com/Fazal/AddTwoNumber.java
@@ -0,0 +1,7 @@
+package com.Fazal;
+public class AddTwoNumber{
+    public int add(int x,int y){
+        return x+y;
+    }
+
+}
---
+++ KodeWala/25th_March/com/Fazal1/SumOfValue.java
@@ -0,0 +1,9 @@
+package com.Fazal1;
+import com.Fazal.AddTwoNumber;
+public class SumOfValue{
+    public static void main(String args[]){
+        AddTwoNumber addTwoNumber = new AddTwoNumber();
+        int output = addTwoNumber.add(10,12 );
+        System.out.println("output = "+output);
+    }
+}
