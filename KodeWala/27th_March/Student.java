6ff3ca29e24dff03f424f2d3c474a4eced8005c1
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-27 12:44:34 +0530

    Student

---
+++ KodeWala/27th_March/Student.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Student{
+    public static void  main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please your name....");
+        String name = sc.next();
+        System.out.println("Your name is :"+name);
+        sc.close();
+    }
+}
