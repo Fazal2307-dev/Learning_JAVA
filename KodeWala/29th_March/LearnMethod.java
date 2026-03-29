48a57e2d10fac215d128cf6fc099a7e28750b90f
Fazal2307-dev <mdfazal2307@gmail.com>
2026-03-29 22:23:53 +0530

    LearnMethod

---
+++ KodeWala/29th_March/LearnMethod.java
@@ -0,0 +1,10 @@
+class LearnMethod{
+    public void Hello(){
+        System.out.println("Hello,KodeWala");
+    }
+    public static void main(String[] args) {
+        LearnMethod m = new LearnMethod();
+            m.Hello();
+    }
+
+}
