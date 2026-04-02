8922ca34b94c84e1661b9e586c1507b1c13dc930
Fazal2307-dev <mdfazal2307@gmail.com>
2026-04-02 17:56:28 +0530

    Driver1

---
+++ KodeWala/02th_April/Driver1.java
@@ -0,0 +1,30 @@
+
+class OrderMgmt {
+    OrderMgmt(){
+        super();
+        System.out.println("OrderMgmt.OrderMgmt()");
+    }
+}
+
+class Order extends OrderMgmt{
+    public Order() {
+        this("Kodewala");
+        System.out.println("Order.Order()");
+
+    }
+    public Order(String name) {
+        super();
+        System.out.println("Order.Order(name)");
+    }
+}
+
+
+
+public class Driver1 extends Object{
+
+    public static void main(String[] args) {
+        Order ord = new Order();
+
+    }
+
+}
