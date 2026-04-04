c8a2799755aab30e280076436ba650e44c4b31c3
Fazal2307-dev <mdfazal2307@gmail.com>
2026-04-04 19:10:03 +0530

    Main

---
+++ KodeWala/04th_April/Main.java
@@ -0,0 +1,22 @@
+class Student {
+    String name;
+    int age;
+
+    // Constructor
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Object creation -> constructor automatically call hoga
+        Student s1 = new Student("Rahul", 20);
+        s1.display();
+    }
+}
