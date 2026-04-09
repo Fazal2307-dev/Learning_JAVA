762067263cc8a2ea5df86642213e3a5382730bd7
Fazal2307-dev <mdfazal2307@gmail.com>
2026-04-09 19:26:36 +0530

    StringLiterals

--- KodeWala/09th_April/stringQ1.java
+++ KodeWala/09th_April/stringQ1.java
@@ -2,6 +2,8 @@ class StringLiterals{
     public static void main(String[] args){
         String s1 = "Kodewala";
         String s2 = "Kodewala";
+        String s3 = "Fazal";
         System.out.println(s1 == s2);
+        System.out.println(s2 == s3);
     }
 }
