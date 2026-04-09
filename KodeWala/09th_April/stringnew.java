4cc0433a5953871de3e573b3ae2c690e19cbe13e
Fazal2307-dev <mdfazal2307@gmail.com>
2026-04-09 19:36:31 +0530

    StringNew

---
+++ KodeWala/09th_April/stringnew.java
@@ -0,0 +1,7 @@
+class StringNew{
+    public static void main(String[] args){
+        String s1 = new String("kodewala");
+        String s2 = new String("Kodewala");
+        System.out.println(s1 == s2);
+    }
+}
