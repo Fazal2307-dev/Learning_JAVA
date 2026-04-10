6f34f7b10c9da53099c390aad3ec10af05c9c39a
Fazal2307-dev <mdfazal2307@gmail.com>
2026-04-10 10:29:50 +0530

    Fazal

--- KodeWala/10th_April/str.java
+++ KodeWala/10th_April/str.java
@@ -1,7 +1,13 @@
 class Str{
     public static void main(String[] args){
-        String s1 = "Fazal";
+        String s1 = "Fazal";//scp
         String s2 = s1.concat(" Anjum");
         System.out.println(s2);
+        String s3 = "Fazal Anjum";
+        System.out.println(s2 == s3);
+        String t = "Faz";
+        String s = t+"al";//heap
+        System.out.println(s);
+        System.out.println(s1 == s);
     }
 }
