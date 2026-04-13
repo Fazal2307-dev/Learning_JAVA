cf0562e2ada8739f34b873059b3255f4199c55c6
Fazal2307-dev <mdfazal2307@gmail.com>
2026-04-13 21:46:17 +0530

    StringBufferExp

---
+++ KodeWala/13th_April/StringBufferExp.java
@@ -0,0 +1,8 @@
+public class StringBufferExp {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer("KodeWala Academy");
+        sb.append(", BTM");
+        sb.append(", Banglore");
+        System.out.println("Details :"+sb);
+    }
+}
