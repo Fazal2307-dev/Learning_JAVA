1ee7ccaa80d405c564fab08fffd546e060c41fdf
Fazal2307-dev <mdfazal2307@gmail.com>
2026-04-28 19:15:58 +0530

    Vehical_getspeed

--- KodeWala/April/28th_April/Interface/Shape_getArea.java
+++ KodeWala/April/28th_April/Interface/Shape_getArea.java
@@ -33,7 +33,7 @@ class Triangle implements Shape{
     private double height;
     Triangle(double base,double height){
         this.base=base;
-        this.height = base;
+        this.height = height;
     }
     @Override
     public double getArea(){
---
+++ KodeWala/April/28th_April/Interface/Vehical_getspeed.java
@@ -0,0 +1,44 @@
+//Create an interface Vehicle:method: double getSpeed()Create classes:
+//Car → speed = distance/time:Bike → speed = distance/time
+//In main:create objects using interface reference print speed
+interface Vehical{
+    public abstract double getSpeed();
+}
+class Car implements Vehical{
+    private double speed;
+    private double time;
+    private double distance;
+    public Car(double distance,double time){
+        this.distance=distance;
+        this.speed=speed;
+        this.time = time;
+    }
+    @Override
+    public double getSpeed(){
+        speed = distance / time;
+        return speed;
+    }
+}
+class Bike implements Vehical{
+    private double speed;
+    private double time;
+    private double distance;
+    public Bike(double distance,double time){
+        this.distance=distance;
+        this.speed=speed;
+        this.time = time;
+    }
+    @Override
+    public double getSpeed(){
+        speed = distance / time;
+        return speed;
+    }
+}
+public class Vehical_getspeed {
+    public static void main(String[] args){
+        Vehical c1 = new Car(5.0,2.0);
+        System.out.println("Car Speed :"+c1.getSpeed());
+        Vehical B1 = new Bike(5.0,2.0);
+        System.out.println("Bike Speed :"+B1.getSpeed());
+    }
+}
