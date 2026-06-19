642df03455d96f06b8741214300160fd374409ff
Fazal2307-dev <mdfazal2307@gmail.com>
2026-06-19 15:44:41 +0530

    Problem_3

--- KodeWala/June/Threads/Day1/Problem_3.java
+++ KodeWala/June/Threads/Day1/Problem_3.java
@@ -1,22 +1,29 @@
 //Write a Java program that creates two threads to find and print even and odd
 // numbers from 1 to 20.
-class MyThread3 extends Thread{
-    public void run(){
-        for(int i=1;i<=20;i++){
-            if( i % 2 ==0){
-                System.out.println("Even no :"+ i + " "+MyThread3.currentThread().getName());
-            }
-        }
-        for(int i =1;i<=20;i++){
-            if(i % 2 !=0){
-                System.out.println("Odd no :"+i +" "+MyThread3.currentThread().getName());
+class evenThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even no :" + i + " " + MyThread3.currentThread().getName());
             }
         }
     }
 }
+    class oddThread extends Thread {
+        public void run() {
+            for (int i = 1; i <= 20; i++) {
+                if (i % 2 != 0) {
+                    System.out.println("Odd no :" + i + " " + MyThread3.currentThread().getName());
+                }
+            }
+        }
+    }
 public class Problem_3 {
     public static void main(String[] args){
-        MyThread3 t1 = new MyThread3();
+        evenThread t1 = new evenThread();
         t1.start();
+
+        oddThread t2 = new oddThread();
+        t2.start();
     }
 }
